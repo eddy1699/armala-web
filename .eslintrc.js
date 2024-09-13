a2d8334b7b1e1f671358c9636fa5e164ddceed75
Edgardo Samame <edgardosamame@gmail.com>
2024-09-13 02:44:59 -0500

    userdone10

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,9 @@ module.exports = {
     requireConfigFile: false
   },
   rules: {
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 'vue/multi-word-component-names': 'off'
-    'no-console': process.env.NODE_ENV === 'production',
-    'no-debugger': process.env.NODE_ENV === 'production',
-    'no-unused-vars': process.env.NODE_ENV === 'production',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off'
   }
 }
