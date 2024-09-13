78821cacaa846271ee1d8f2573b70f5507a0be3a
Edgardo <edgardosamame@gmail.com>
2024-09-13 11:03:00 -0500

    from mac2

--- src/views/full-page/LoginView.vue
+++ src/views/full-page/LoginView.vue
@@ -1,6 +1,13 @@
 <template>
-  <card-component title="Iniciar Sesión" icon="lock">
-    <router-link slot="button" to="/" class="button is-small">
+  <card-component
+    title="Iniciar Sesión"
+    icon="lock"
+  >
+    <router-link
+      slot="button"
+      to="/"
+      class="button is-small"
+    >
       Dashboard
     </router-link>
     <img
@@ -9,10 +16,18 @@
       height="250"
       width="250"
       class="center"
-    />
-    <form method="POST" @submit.prevent="login">
+    >
+    <form
+      method="POST"
+      @submit.prevent="login"
+    >
       <b-field label="Usuario">
-        <b-input v-model="form.email" name="user" type="text" required />
+        <b-input
+          v-model="form.email"
+          name="user"
+          type="text"
+          required
+        />
       </b-field>
 
       <b-field label="Contraseña">
@@ -28,12 +43,16 @@
         data-sitekey="6Lfm3FYoAAAAAOTGDrnZcGShmF3oodUREPrAyA9r"
       />
       <b-field>
-        <b-checkbox v-model="form.remember" type="is-black" class="is-thin">
+        <b-checkbox
+          v-model="form.remember"
+          type="is-black"
+          class="is-thin"
+        >
           Recordarme
         </b-checkbox>
       </b-field>
 
-      <hr />
+      <hr>
 
       <b-field>
         <div class="control">
@@ -72,38 +91,38 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import CardComponent from "@/components/CardComponent.vue";
-import "@/scss/test.scss";
-import backendURL from "@/url";
-import axios from "axios";
+import { defineComponent } from 'vue'
+import CardComponent from '@/components/CardComponent.vue'
+import '@/scss/test.scss'
+import backendURL from '@/url'
+import axios from 'axios'
 
-import isAuthenticatedGuard from "../../router/auth-guard";
+import isAuthenticatedGuard from '../../router/auth-guard'
 export default defineComponent({
-  name: "LoginView",
+  name: 'LoginView',
   components: { CardComponent },
-  data() {
+  data () {
     return {
       isLoading: false,
       formCompleted: false,
       form: {
-        email: "",
-        password: "",
-        remember: false,
-      },
-    };
+        email: '',
+        password: '',
+        remember: false
+      }
+    }
   },
   watch: {
-    "form.email": function () {
-      this.inputsFilled(this.form.email, this.form.password);
+    'form.email': function () {
+      this.inputsFilled(this.form.email, this.form.password)
     },
-    "form.password": function () {
-      this.inputsFilled(this.form.email, this.form.password);
-    },
+    'form.password': function () {
+      this.inputsFilled(this.form.email, this.form.password)
+    }
   },
-  created() {
-    this.validateToken();
-    this.loadAccess();
+  created () {
+    this.validateToken()
+    this.loadAccess()
   },
   methods: {
     // submit () {
@@ -115,84 +134,84 @@ export default defineComponent({
     //     this.$router.push('/')
     //   }, 750)
     // },
-    async onloadCallback() {
-      alert("grecaptcha is ready!");
+    async onloadCallback () {
+      alert('grecaptcha is ready!')
     },
-    inputsFilled(username, password) {
+    inputsFilled (username, password) {
       if (username.length > 1 && password.length > 1) {
-        this.formCompleted = true;
+        this.formCompleted = true
       } else {
-        this.formCompleted = false;
+        this.formCompleted = false
       }
     },
-    async login() {
-      this.isLoading = true;
+    async login () {
+      this.isLoading = true
       const response = await axios.post(`${backendURL}/api/User/login`, {
         userName: this.form.email,
-        password: this.form.password,
-      });
-      if (response.data.isSuccess && response.data.result.token !== "") {
-        console.log("No hay token");
+        password: this.form.password
+      })
+      if (response.data.isSuccess && response.data.result.token !== '') {
+        console.log('No hay token')
         await localStorage.setItem(
-          "userData",
+          'userData',
           JSON.stringify(response.data.result)
-        );
+        )
         const userCompleteName = JSON.parse(
-          localStorage.getItem("userData")
-        ).user;
-        console.log("userloging", userCompleteName.name);
-        this.saveAccess();
-        this.$router.push("/home");
+          localStorage.getItem('userData')
+        ).user
+        console.log('userloging', userCompleteName.name)
+        this.saveAccess()
+        this.$router.push('/home')
         this.$buefy.toast.open({
           duration: 5000,
           message: `Bienvenido ${userCompleteName.name}`,
-          position: "is-top",
-          type: "is-success",
-        });
+          position: 'is-top',
+          type: 'is-success'
+        })
       } else {
-        this.isLoading = false;
+        this.isLoading = false
         this.$buefy.toast.open({
           duration: 5000,
-          message: "Usuario o contraseña incorrectos",
-          position: "is-bottom",
-          type: "is-danger",
-        });
+          message: 'Usuario o contraseña incorrectos',
+          position: 'is-bottom',
+          type: 'is-danger'
+        })
       }
-      this.info = response.data.result;
-      return this.info;
+      this.info = response.data.result
+      return this.info
     },
-    validateToken() {
+    validateToken () {
       try {
         const tokenLocalStorage = JSON.parse(
-          localStorage.getItem("userData")
-        ).token;
+          localStorage.getItem('userData')
+        ).token
         if (tokenLocalStorage != null) {
-          this.$router.push("/clientes");
+          this.$router.push('/clientes')
         }
       } catch (error) {
         // console.warn(error)
       }
     },
-    saveAccess() {
-      console.log("Guardado!", this.form.remember);
+    saveAccess () {
+      console.log('Guardado!', this.form.remember)
       if (this.form.remember) {
         const userAccess = {
           userName: this.form.email,
-          password: this.form.password,
-        };
-        localStorage.setItem("userAccess", JSON.stringify(userAccess));
-        console.log("Guardado!");
+          password: this.form.password
+        }
+        localStorage.setItem('userAccess', JSON.stringify(userAccess))
+        console.log('Guardado!')
       }
     },
-    loadAccess() {
-      if (localStorage.getItem("userAccess")) {
+    loadAccess () {
+      if (localStorage.getItem('userAccess')) {
         this.form.email = JSON.parse(
-          localStorage.getItem("userAccess")
-        ).userName;
+          localStorage.getItem('userAccess')
+        ).userName
       }
-    },
-  },
-});
+    }
+  }
+})
 </script>
 
 <style lang="scss" scoped>
